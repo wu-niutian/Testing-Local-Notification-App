b4328316c945fb0b920e481ab469b0b32db8324d
Wu Niutian <wuniutian@gmail.com>
2022-11-13 11:17:24 +0800

    Made a Local Notification for SAP'22 App

--- Testing Local Notification App/ContentView.swift
+++ Testing Local Notification App/ContentView.swift
@@ -6,16 +6,34 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Request Permisson") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All Set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            Button("Scheduled Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Check Your Streak's Progress!"
+                content.subtitle = "Click here to repeat the Streak"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 86400, repeats: true)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+            
         }
-        .padding()
     }
 }
 
