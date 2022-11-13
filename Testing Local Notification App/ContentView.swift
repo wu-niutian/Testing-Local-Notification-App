cd1f5f7a35120d7b644fd020a84c575198e94fb4
Wu Niutian <wuniutian@gmail.com>
2022-11-13 10:47:21 +0800

    Initial Commit

---
+++ Testing Local Notification App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Testing Local Notification App
+//
+//  Created by    wuniutian on 13/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Testing Local Notification App/Testing_Local_Notification_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_Local_Notification_AppApp.swift
+//  Testing Local Notification App
+//
+//  Created by    wuniutian on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Testing_Local_Notification_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
